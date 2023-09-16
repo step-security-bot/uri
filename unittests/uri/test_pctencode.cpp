2d4fc705a3cd151f403dc51049bfbcb5568e7f98
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-09-16 22:11:31 +0200

    Experimenting with FuzzTest.

--- unittests/uri/test_pctencode.cpp
+++ unittests/uri/test_pctencode.cpp
@@ -1,9 +1,14 @@
-#include <gtest/gtest.h>
-
 #include <iterator>
 #include <string_view>
 
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "uri/pctdecode.hpp"
 #include "uri/pctencode.hpp"
+
+#if URI_FUZZTEST
+#include "fuzztest/fuzztest.h"
+#endif
 
 using namespace std::string_view_literals;
 
@@ -13,4 +18,22 @@ TEST (PctEncode, Hello) {
   uri::pctencode (std::begin (input), std::end (input),
                   std::back_inserter (out));
   EXPECT_EQ (out, "Hello");
+}
+
+static void pctcodec_equals_original (std::string const& s) {
+  std::string encoded;
+  uri::pctencode (std::begin (s), std::end (s), std::back_inserter (encoded));
+
+  std::string out;
+  std::copy (uri::pctdecode_begin (encoded), uri::pctdecode_end (encoded),
+             std::back_inserter (out));
+
+  EXPECT_THAT (out, testing::StrEq (s));
+}
+#if URI_FUZZTEST
+FUZZ_TEST (TagUtilsFuzzTests, pctcodec_equals_original);
+#endif
+
+TEST (TagUtilsFuzzTests, pctcodec_equals_originalRegression) {
+  pctcodec_equals_original ("");
 }
