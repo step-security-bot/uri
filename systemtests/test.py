4673a75789e0967a09c87492a3d836811bd37bee
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-10-18 10:40:06 +0200

    Resolve mypy warnings.

--- systemtests/test.py
+++ systemtests/test.py
@@ -15,33 +15,36 @@ import argparse
 import sys
 import uritools
 
+def asstr (x):
+    return '"' + x + '"' if x is not None else None
+
+
 def parse (s):
-  print('URI:', s)
-  asstr = lambda x: '"' + x + '"' if x is not None else None
-  
-  u = uritools.urisplit(s)
-  print(' scheme: {0}'.format(asstr(u.scheme)))
-  print(' userinfo: {0}'.format(asstr(u.userinfo)))
-  print(' host: {0}'.format(asstr(u.host)))
-  print(' port: {0}'.format(asstr(u.port)))
-  print(' path: {0}'.format(asstr(u.path)))
-  print(' query: {0}'.format(asstr(u.query)))
-  print(' fragment: {0}'.format(asstr(u.fragment)))
+    print('URI:', s)
+    parts = uritools.urisplit(s)
+    print(f" scheme: {asstr(parts.scheme)}")
+    print(f" userinfo: {asstr(parts.userinfo)}")
+    print(f" host: {asstr(parts.host)}")
+    print(f" port: {asstr(parts.port)}")
+    print(f" path: {asstr(parts.path)}")
+    print(f" query: {asstr(parts.query)}")
+    print(f" fragment: {asstr(parts.fragment)}")
+
 
 def parse_file (path):
     with open(path) as f:
-        lines = f.readlines()
-        for s in lines:
-            parse(s)
-            
-def main (args):
-  parser = argparse.ArgumentParser (description='URI parser wrapper')
-  #parser.add_argument('infile', help='The name of a file containing URI text')
-  parser.add_argument('binary', help='The path of the binary to be tested')
-  parser.add_argument('tests', help='The path of the directory containing test inputs')
+        for line in f.readlines():
+            parse(line)
 
-  options = parser.parse_args(args)
-  parse_file (options.infile)
-  
+
+def main (args):
+    parser = argparse.ArgumentParser (description="URI parser wrapper")
+    parser.add_argument("binary", help="The path of the binary to be tested")
+    parser.add_argument("tests", help="The path of the directory containing test inputs")
+
+    options = parser.parse_args(args)
+    parse_file (options.infile)
+
+
 if __name__ == '__main__':
-  sys.exit(main (sys.argv[1:]))
+    sys.exit(main (sys.argv[1:]))
